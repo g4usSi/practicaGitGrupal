0d1a53ed5e9b52d938673576473380e91a8bb1d4
Geovanny <geovanny.alcon2@gmail.com>
2024-07-18 23:56:52 -0600

    Correccion en el manejo de excepciones

--- practicaGrupal/Class1.cs
+++ practicaGrupal/Class1.cs
@@ -10,55 +10,59 @@ namespace practicaGrupal
     {
         double numeroPrimero { get; set; }
         double numeroSegundo { get; set; }
-// METODO ALTERADO ----------------------------------------------------------------------------------
+        // METODO ALTERADO ----------------------------------------------------------------------------------
         public void Pedir()
         {
             bool condicion = true;
-            while(condicion){
+            double numeroIntroducido1 = 0, numeroIntroducido2 = 0;
 
-                double numeroIntroducido1 = 0, numeroIntroducido2 = 0;
-                    do
+            while (condicion)
+            {
+                do
+                {
+                    Console.Write("> Introduzca el primer número: ");
+                    try
                     {
-                    Console.Write("> Introduzca el primer numero: ");
-                        try
-                        {
-                            numeroIntroducido1 = Convert.ToInt32(Console.ReadLine());
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine("ERROR INESPERADO, VERIFIQUE LA ENTRADA... \n" + ex);
-                            Console.WriteLine();
-                        }
-
-                    Console.Write("> Introduzca el segundo numero: ");
-                        try
-                        {
-                            numeroIntroducido2 = Convert.ToInt32(Console.ReadLine());
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine("ERROR INESPERADO, VERIFIQUE LA ENTRADA... \n" + ex);
-                            Console.WriteLine();
-                        }
-                    if ((numeroIntroducido1 > 0) && (numeroIntroducido2 > 0))
-                    {
-                       condicion = false;
+                        numeroIntroducido1 = Convert.ToDouble(Console.ReadLine());
                     }
-                    } while ((numeroIntroducido1 <= 0) && (numeroIntroducido2 <= 0));
-                
-                numeroPrimero = numeroIntroducido1;
-                numeroSegundo = numeroIntroducido2;
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("ERROR: Entrada no válida. Debe ingresar un número.\n");
+                        continue;
+                    }
+
+                    Console.Write("> Introduzca el segundo número: ");
+                    try
+                    {
+                        numeroIntroducido2 = Convert.ToDouble(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("ERROR: Entrada no válida. Debe ingresar un número.\n");
+                        continue;
+                    }
+
+                    if (numeroIntroducido1 > 0 && numeroIntroducido2 > 0)
+                    {
+                        condicion = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ambos números deben ser mayores que cero. Por favor, intente nuevamente.\n");
+                    }
+                } while (condicion);
             }
+            numeroPrimero = numeroIntroducido1;
+            numeroSegundo = numeroIntroducido2;
             Console.WriteLine();
         }
-//HASTA ACA -------------------------------------------------------------------------------------------------
-//Jenny
-public void Suma()
-{
+        //HASTA ACA -------------------------------------------------------------------------------------------------
+        //Jenny
+    public void Suma()
+    {
     double resultado= numeroPrimero+ numeroSegundo;
     Console.WriteLine("El resultado de la suma es: "+resultado);
-   
-}
+    }
 public void Resta()
 {
     double resultado = numeroPrimero - numeroSegundo;
